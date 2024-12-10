fbbae92fa6d33335cd251e068aa8051231de705f
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-10 11:16:15 +0100

    well try this in a minute

---
+++ models/seeder1.js
@@ -0,0 +1,31 @@
+const fs = require('fs').promises;
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const Post = require("./postModel");
+const fname = process.argv[2] ?? "posts1.json";
+
+const importData = async (users) => {
+    try {
+      await Post.create(users);
+      console.log(`Data successfully created from json file ${fname}!`);
+    } catch (err) {
+      console.log(err);
+    }
+    process.exit();
+  };
+ 
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        
+        console.log("seeder running");
+        
+        const data = await fs.readFile(`${__dirname}/${fname}`, 'utf-8');
+        const posts = JSON.parse(data);
+        importData(posts);
+    } catch (err) {
+      console.log('error: ' + err)
+    }
+  })()

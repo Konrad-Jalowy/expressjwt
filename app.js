4faeacafdba15a96ddb7de2025f0463e5d5eddda
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-10 11:43:44 +0100

    login skeleton

--- app.js
+++ app.js
@@ -8,4 +8,10 @@ app.get("/", (req, res) => {
     res.json({"hello": "world"});
 });
 
+app.post("/login", (req, res) => {
+    const username = req.body.username
+    const _user = { name: username }
+    res.json({"user": _user});
+});
+
 module.exports = app;

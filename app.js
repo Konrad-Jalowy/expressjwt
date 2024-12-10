0eefd551a81addd58b5a54def39f32cbe19e5793
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-10 11:19:34 +0100

    skeleton

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 require('dotenv').config()
 
 const express = require('express')
-const app = express()
+const app = express();
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({"hello": "world"});
+});
+
+module.exports = app;

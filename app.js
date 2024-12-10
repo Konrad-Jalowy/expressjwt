f6e66e7927cc3091d2f3efefc5a90740aba201a2
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-10 13:10:46 +0100

    a little more mvc

--- app.js
+++ app.js
@@ -16,41 +16,18 @@ app.get("/tokenprotected", MainController.authToken, (req, res) => {
     return res.json({"msg": "you sent correct token!"});
 })
 
-app.get("/all", catchAsync(async (req, res,next) => {
-    let _posts = await Post.find({});
-    return res.json({"posts": _posts});
-}));
+app.get("/all", MainController.all);
 
-app.get('/user-all', MainController.authToken, catchAsync(async (req, res, next) => {
-    let _username = req.user.name;
-    let _posts =   await Post.find({"author": _username});
-    return res.json({"your_usenmare": _username, "your_posts": _posts});
-  }));
+app.get('/user-all', MainController.authToken, MainController.userAll);
 
 app.post("/login", MainController.login);
 
-app.post('/token', (req, res) => {
-    const refreshToken = req.body.token
-    if (refreshToken == null) return res.sendStatus(401)
-    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
-    jwt.verify(refreshToken, process.env.REF_TOKEN_SECRET, (err, user) => {
-      if (err) return res.sendStatus(403)
-      const accessToken = generateAccessToken({ name: user.name })
-      res.json({ accessToken: accessToken })
-    });
-  });
+app.post('/token', MainController.token);
 
 app.delete('/logout', MainController.delete);
+
 app.use(MainController.errHandler);
+
 app.get("*", MainController.notFound);
-
-function generateAccessToken(user){
-    return jwt.sign(user, process.env.SECRET_KEY, { expiresIn: '15s' })
-}
-
-function generateRefreshToken(user){
-    return jwt.sign(_user, process.env.REF_TOKEN_SECRET)
-}
-
 
 module.exports = app;
--- mainController.js
+++ mainController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-
+const catchAsync = require("./catchAsync");
 let refreshTokens = [];
 
 exports.notFound = function(req, res){
@@ -35,6 +35,28 @@ exports.login = (req, res) => {
     refreshTokens.push(refreshToken)
     res.json({"user": _user, "accessToken": accessToken, "refreshToken": refreshToken})
 }
+
+exports.all = catchAsync(async (req, res,next) => {
+    let _posts = await Post.find({});
+    return res.json({"posts": _posts});
+});
+
+exports.userAll = catchAsync(async (req, res, next) => {
+    let _username = req.user.name;
+    let _posts =   await Post.find({"author": _username});
+    return res.json({"your_usenmare": _username, "your_posts": _posts});
+  });
+
+exports.token = (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REF_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403)
+      const accessToken = generateAccessToken({ name: user.name })
+      res.json({ accessToken: accessToken })
+    });
+  };
 
 
 function generateAccessToken(user){

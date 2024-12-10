2f945aef612c046310fd675eede70fe65cec69cd
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-10 11:54:39 +0100

    were getting somewhere

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 app.use(express.json())
 
@@ -11,7 +12,8 @@ app.get("/", (req, res) => {
 app.post("/login", (req, res) => {
     const username = req.body.username
     const _user = { name: username }
-    res.json({"user": _user});
+    const accessToken = jwt.sign(_user, process.env.SECRET_KEY);
+    res.json({"user": _user, "accessToken": accessToken});
 });
 
 module.exports = app;

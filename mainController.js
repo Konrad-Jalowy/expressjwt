3fdc86a02a72af63bc0f141ee3d41c0a0d838fc2
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-10 12:58:01 +0100

    a little mvc

--- mainController.js
+++ mainController.js
@@ -5,3 +5,12 @@ exports.notFound = function(req, res){
 exports.errHandler = (err, req, res, next) => {
     res.status(500).json({"Error": "Some kind of error occurred."});
 }
+
+
+function generateAccessToken(user){
+    return jwt.sign(user, process.env.SECRET_KEY, { expiresIn: '15s' })
+}
+
+function generateRefreshToken(user){
+    return jwt.sign(_user, process.env.REF_TOKEN_SECRET)
+}

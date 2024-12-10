7c19d85bf995e62a703d75c8aac53aa2c50a1e64
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-10 14:23:02 +0100

    old code preserved

--- oldCode.js
+++ oldCode.js
@@ -13,3 +13,15 @@ exports.authToken = function authenticateToken(req, res, next) {
       });
     
   }
+
+  
+exports.token = (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REF_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403)
+      const accessToken = generateAccessToken({ name: user.name })
+      res.json({ accessToken: accessToken })
+    });
+  };

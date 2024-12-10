d34d9dd8625f7af629fb3df59985f4730c16fff7
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-10 11:20:45 +0100

    skeleton working

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-require('dotenv').config()
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = require("./app.js");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.SERVER_PORT;
+
+const server = app.listen(port, () => {
+  console.log(`App running on port http://localhost:${port}...`);
+});

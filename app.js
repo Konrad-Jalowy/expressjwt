29e1896d643b196a06e94672f7518b5e50b469a8
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-10 11:58:26 +0100

    were getting somewhere

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express();
 const jwt = require('jsonwebtoken');
+const Post = require('./models/postModel');
 require('dotenv').config();
 
 app.use(express.json())
 
 app.get("/", (req, res) => {
     res.json({"hello": "world"});
+});
+
+app.get("/all", async (req, res) => {
+    let _posts = await Post.find({});
+    return res.json({"posts": _posts});
 });
 
 app.post("/login", (req, res) => {
